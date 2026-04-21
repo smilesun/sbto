8f378760bf31928271aa436fa0cfcf6ff566d05f
smilesun <smilesun.east@gmail.com>
2026-04-21 14:45:52 +0200

    .

--- run_cem.sh
+++ run_cem.sh
@@ -1,1 +1,1 @@
-uv run sbto/main.py solver=cem task.cfg_ref.motion_path=datasets/robot-object/sub10_largebox_000_original.npz
+uv run sbto/main.py solver=cem task.cfg_ref.motion_path=datasets/robot-object/sub10_largebox_000_original.npz task/g1/sim/mj_scene@task.sim.mj_scene=box
